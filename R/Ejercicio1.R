8275c543b863934c1d4c87efbbf48333f74a99e3
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-09-08 13:27:54 -0600

    Brief R review

--- R/Ejercicio1.R
+++ R/Ejercicio1.R
@@ -253,3 +253,11 @@ head(sizeFactors(sce))
 ## "Manualmente" reemplaza los factores de tamaño
 sizeFactors(sce) <- scater::librarySizeFactors(sce)
 head(sizeFactors(sce))
+
+
+### Información de la sesión de R ###
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
+
