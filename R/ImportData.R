d1f5bac819874e46359bd919785aee7a7f5d3e4f
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-09-12 13:54:38 -0600

    Download scPipe output files to SCE

--- R/ImportData.R
+++ R/ImportData.R
@@ -1,5 +1,6 @@
 ########## Import Data ##########
 
+
 ### CellRanger ###
 
 ## Descargar los datos de ejemplo procesados con CellRanger
@@ -8,13 +9,15 @@
 library("BiocFileCache")
 
 bfc <- BiocFileCache()
-pbmc.url <-
+
+## Crear una URL de descarga para el archivo tar.gz
   paste0(
     "http://cf.10xgenomics.com/samples/cell-vdj/",
     "3.1.0/vdj_v1_hs_pbmc3/",
     "vdj_v1_hs_pbmc3_filtered_feature_bc_matrix.tar.gz"
   )
 
+##  bfcrpath para descargar el archivo ZIP desde la URL
 pbmc.data <- bfcrpath(bfc, pbmc.url)
 
 ## Extraer los archivos en un directorio temporal
@@ -23,7 +26,7 @@ untar(pbmc.data, exdir = tempdir())
 ## Enumerar los archivos que descargamos y que extrajimos
 pbmc.dir <- file.path(
   tempdir(),
-  "filtered_feature_bc_matrix"
+  "filtered_feature_bc_matrix" # Estos son los archivos típicos de CellRanger
 )
 
 list.files(pbmc.dir)
@@ -47,3 +50,42 @@ sce.pbmc
 
 ## ¿Qué tan grande es el objeto de R (En MB)?
 lobstr::obj_size(sce.pbmc) / 1024^2
+
+
+### scPipe ###
+
+## Descargar datos de ejemplo procesados con scPipe
+library("BiocFileCache")
+
+bfc <- BiocFileCache()
+
+sis_seq.url <-
+  "https://github.com/LuyiTian/SIS-seq_script/archive/master.zip"
+
+##  bfcrpath para descargar el archivo ZIP desde la URL
+sis_seq.data <- bfcrpath(bfc, sis_seq.url)
+
+## Extraer los archivos en un directorio temporal
+unzip(sis_seq.data, exdir = tempdir())
+
+## Enumerar algunos de los archivos que descargamos y extrajimos
+sis_seq.dir <- file.path(
+  tempdir(),
+  "SIS-seq_script-master", # Estos son los archivos típicos de scPipe
+  "data",
+  "BcorKO_scRNAseq",
+  "RPI10"
+)
+
+list.files(sis_seq.dir)
+
+## Importar los datos como un objeto de tipo SingleCellExperiment
+library("scPipe")
+
+sce.sis_seq <- create_sce_by_dir(sis_seq.dir)
+
+## Revisar el objeto que acabamos de construir
+sce.sis_seq
+
+## ¿Qué tan grande es el objeto de R (En MB)?
+lobstr::obj_size(sce.sis_seq) / 1024^2
