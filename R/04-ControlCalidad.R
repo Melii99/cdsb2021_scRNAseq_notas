85f9b8f0daf9a2e15e45d8d1dec76d17dae1dbcc
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-09-14 03:40:48 -0600

    Ejercicio filtrado de células

--- R/04-ControlCalidad.R
+++ R/04-ControlCalidad.R
@@ -88,3 +88,112 @@ plotColData(sce.416b,
             other_fields = "phenotype"
 ) +
   facet_wrap(~phenotype)
+
+
+### Eliminar células de baja calidad ###
+
+## Valores de límite ejemplo
+qc.lib <- sce.416b$sum < 100000
+qc.nexprs <- sce.416b$detected < 5000
+qc.spike <- sce.416b$altexps_ERCC_percent > 10
+qc.mito <- sce.416b$subsets_Mito_percent > 10
+discard <- qc.lib | qc.nexprs | qc.spike | qc.mito
+
+## Obtener un resumen del número de células eliminadas por cada filtro
+DataFrame(
+  LibSize = sum(qc.lib),
+  NExprs = sum(qc.nexprs),
+  SpikeProp = sum(qc.spike),
+  MitoProp = sum(qc.mito),
+  Total = sum(discard)
+)
+
+## Usando isOutlier() para determinar los valores de corte
+
+qc.lib2 <- isOutlier(sce.416b$sum, log = TRUE, type = "lower")
+
+qc.nexprs2 <- isOutlier(sce.416b$detected,
+                        log = TRUE,
+                        type = "lower"
+)
+
+qc.spike2 <- isOutlier(sce.416b$altexps_ERCC_percent,
+                       type = "higher"
+)
+
+qc.mito2 <- isOutlier(sce.416b$subsets_Mito_percent,
+                      type = "higher"
+)
+
+discard2 <- qc.lib2 | qc.nexprs2 | qc.spike2 | qc.mito2
+
+## Extraer los límites de valores (thresholds)
+attr(qc.lib2, "thresholds")
+
+attr(qc.nexprs2, "thresholds")
+
+## Obtener un resumen del número de células eliminadas por cada filtro
+DataFrame(
+  LibSize = sum(qc.lib2),
+  NExprs = sum(qc.nexprs2),
+  SpikeProp = sum(qc.spike2),
+  MitoProp = sum(qc.mito2),
+  Total = sum(discard2)
+)
+
+## Más pruebas
+plotColData(sce.416b,
+            x = "block",
+            y = "detected",
+            other_fields = "phenotype"
+) +
+  scale_y_log10() +
+  facet_wrap(~phenotype)
+
+## Determinar el bloque (batch) de muestras (fonotipo-bloque)
+batch <- paste0(sce.416b$phenotype, "-", sce.416b$block)
+
+## Versión de isOutlier() que toma en cuenta los bloques de muestras
+qc.lib3 <- isOutlier(sce.416b$sum,
+                     log = TRUE,
+                     type = "lower",
+                     batch = batch
+)
+
+qc.nexprs3 <- isOutlier(sce.416b$detected,
+                        log = TRUE,
+                        type = "lower",
+                        batch = batch
+)
+
+qc.spike3 <- isOutlier(sce.416b$altexps_ERCC_percent,
+                       type = "higher",
+                       batch = batch
+)
+
+qc.mito3 <- isOutlier(sce.416b$subsets_Mito_percent,
+                      type = "higher",
+                      batch = batch
+)
+
+discard3 <- qc.lib3 | qc.nexprs3 | qc.spike3 | qc.mito3
+
+## Extraer los límites de valores (thresholds)
+attr(qc.lib3, "thresholds")
+
+attr(qc.nexprs3, "thresholds")
+
+## Obtener un resumen del número de células eliminadas por cada filtro
+DataFrame(
+  LibSize = sum(qc.lib3),
+  NExprs = sum(qc.nexprs3),
+  SpikeProp = sum(qc.spike3),
+  MitoProp = sum(qc.mito3),
+  Total = sum(discard3)
+)
+
+### Ejercicio: filtrado de células ###
+
+# R. Sí fue necesario qc.lib para crear discard
+# ¿Cúal filtro fue más estricto? ¿discard o discard2? R. discard
+# Al considerar el grupo de cada muestra (batch), ¿descartamos más células usando un valor de límite automático? R. si
