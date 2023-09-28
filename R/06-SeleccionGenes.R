b94bbf5e7814432a627db0b478bea6c83f651679
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-09-27 22:55:08 -0600

    Quedándonos sólo con los HGVs y SessionInfo

--- R/06-SeleccionGenes.R
+++ R/06-SeleccionGenes.R
@@ -592,3 +592,9 @@ sce.pbmc.hvg
 
 ## Objerto SCE original
 altExp(sce.pbmc.hvg, "original")
+
+### Información de la sesión de R ###
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
