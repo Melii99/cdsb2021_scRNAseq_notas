1f5ed373751eb87933163dcf71b2f642720c5a0a
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-10-04 16:56:30 -0600

    Usando el log-fold change como punto de corte

--- R/09-IdentGenesMarcadores.R
+++ R/09-IdentGenesMarcadores.R
@@ -196,8 +196,8 @@ plotExpression(sce.pbmc,
 ## diferencialmente expresados entre éste y cualquier (any) otro clúster
 
 ## Usadas para todos los genes
-scran::pairwiseTTests()
-scran::combineMarkers()
+#scran::pairwiseTTests()
+#scran::combineMarkers()
 
 ## Usando findMarkers() encontrar marcadores diferenciales entre grupos en datos de células individuales
 library(scran)
@@ -235,3 +235,60 @@ pheatmap(logFCs, breaks = seq(-5, 5, length.out = 101))
 
 ## Usamos el campo Top para identificar un conjunto de genes que distinguen el
 ## clúster 9 de cualquier otro clúster !!!
+
+
+
+### Log-fold change ###
+
+
+## Sin espeficiar el log-fold change ##
+
+## Para cada clúster, usa pruebas t de Welch para identificar los genes que están
+## sobreexpresados entre éste y cualquier otro clúster
+
+## Identificar los genes que tienen una expresión significativamente mayor que otros clústeres
+markers.pbmc.up <- findMarkers(sce.pbmc,
+                               groups = sce.pbmc$cluster,
+                               test.type = "t", direction = "up", pval.type = "any"
+)
+## Almacenar los genes upregulated
+interesting.up <- markers.pbmc.up[[chosen]]
+
+
+## Especificando el lfc ##
+
+## Para cada clúster, usa pruebas t de Welch para identificar los genes que están
+## sobreexpresados con un log-fold change (lfc) o al menos 1 entre éste y cualquier otro clúster
+
+## Identificar los genes que tienen una expresión significativamente mayor que otros clústeres
+## Pero ahora, con lfc = 1 se establece un punto de corte (reduciendo la lista)
+markers.pbmc.up2 <- findMarkers(sce.pbmc,
+                                groups = sce.pbmc$cluster,
+                                test.type = "t", direction = "up", lfc = 1, pval.type = "any"
+)
+
+interesting.up2 <- markers.pbmc.up2[[chosen]]
+
+## La prueba t también nos permite especificar un log-fold change diferente de
+## cero como la hipótesis nula
+
+## Es más riguroso que simplemente filtrar por log-fold change TREAT
+
+
+## Heatmap ##
+
+best.set <- interesting.up2[interesting.up2$Top <= 5, ]
+logFCs <- as.matrix(best.set[, -(1:3)])
+colnames(logFCs) <- sub("logFC.", "", colnames(logFCs))
+pheatmap(logFCs, breaks = seq(-5, 5, length.out = 101))
+
+
+## Los promedios están más centrados en un conjunto de genes marcadores candidatos
+## que están sobreexpresados en el clúster 9
+
+## El incremento del rigor no se da sin costo
+
+## Si el lfc es muy grande podría descartar genes útiles (punto de corte muy estricto)
+
+## E.g., un gen sobreexpresado en una proporción pequeña de células en un clúster sigue
+## siendo un marcador efectivo si el foco está en la especificidad más que en la sensibilidad
